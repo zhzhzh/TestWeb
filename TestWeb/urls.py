6d85b81aadde27534737b7fbec8cd1e600e93572
Zhang, Jie <jzhang14@paypal.com>
2015-12-28 13:44:02 +0800

    change main to angular

--- TestWeb/urls.py
+++ TestWeb/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^angular/', include('angular.urls', namespace='angular')),
 ]
--- angular/views.py
+++ angular/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'angular/index.html')
 
