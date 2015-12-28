d2a8953075e5894dd1c8dcd82be9d0e07b74d72b
Zhang, Jie <jzhang14@paypal.com>
2015-12-28 13:41:14 +0800

    update

--- main/static/main/test_app.js
+++ main/static/main/test_app.js
@@ -22,21 +22,91 @@ app.directive('hello', function(){
     };
 });
 
+//app.directive('expander', function() {
+//    return {
+//        restrict: 'EA',
+//        replace: true,
+//        transclude: true,
+//        scope: { title: '@expanderTitle' },
+//        template: '<div>' +
+//              '<div class="title" ng-click="toggle()">{{title}}</div>' +
+//              '<div class="body" ng-show="showMe" ng-transclude></div>' +
+//              '</div>',
+//        link: function(scope, element, attrs) {
+//            scope.showMe = false;
+//
+//            scope.toggle = function toggle() {
+//                scope.showMe = !scope.showMe;
+//            }
+//        }
+//    }
+//});
+
+//app.directive('expander', function() {
+//    return {
+//        restrict: 'EA',
+//        replace: true,
+//        transclude: true,
+//        scope: { title: '=expanderTitle' },
+//        template: '<div>' +
+//              '<div class="title"">{{title}}</div>' +
+//              '<div class="body closed" ng-transclude></div>' +
+//              '</div>',
+//        link: function(scope, element, attrs) {
+//            var titleElement = angular.element(element.children().eq(0));
+//            var bodyElement = angular.element(element.children().eq(1));
+//
+//            titleElement.bind('click', toggle);
+//            //scope.showMe = false;
+//
+//            function toggle() {
+//                bodyElement.toggleClass('closed');
+//            }
+//        }
+//    }
+//});
+
+app.directive('accordion', function() {
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        controller: function () {
+            var expanders = [];
+            this.gotOpened = function(selectedExpander) {
+                angular.forEach(expanders, function(expander) {
+                   if (selectedExpander != expander) {
+                       expander.showMe = false;
+                   }
+                });
+            };
+
+            this.addExpander = function (expander) {
+                expanders.push(expander);
+            }
+        }
+    }
+});
+
 app.directive('expander', function() {
     return {
         restrict: 'EA',
         replace: true,
         transclude: true,
-        scope: {title: '=expanderTitle'},
+        require: '^?accordion',
+        scope: { title: '=expanderTitle' },
         template: '<div>' +
               '<div class="title" ng-click="toggle()">{{title}}</div>' +
               '<div class="body" ng-show="showMe" ng-transclude></div>' +
               '</div>',
-        link: function(scope, element, attrs) {
+        link: function(scope, element, attrs, accordionController) {
             scope.showMe = false;
+            accordionController.addExpander(scope);
 
             scope.toggle = function toggle() {
                 scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
             }
         }
     }
@@ -45,4 +115,19 @@ app.directive('expander', function() {
 app.controller('TestCtrl', function($scope) {
     $scope.title = 'Click me to expand';
     $scope.text = 'Hi there folks, I am the content<br>that wat hidden but is now shown';
+
+    $scope.expanders = [
+        {
+            title: 'Click me to expand',
+            text: 'Hi there folks, I am the content that was hidden but is now shown.'
+        },
+        {
+            title: 'Click this',
+            text: 'I am even better text than you have seen previously'
+        },
+        {
+            title: 'No, click me!',
+            text: 'I am text that should be seen before seeing other texts'
+        }
+    ];
 });
